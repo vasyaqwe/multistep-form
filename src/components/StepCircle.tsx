2feac61c113216ac38e948da3d64ae9e2273b373
vasyaqwe <elekton.fafik@gmail.com>
2023-03-02 09:12:59 +0200

    fix aria-current ts error

--- src/components/StepCircle.tsx
+++ src/components/StepCircle.tsx
@@ -2,12 +2,12 @@ import { useStore } from "../useStore"
 
 export const StepCircle = ({ num }: { num: number }) => {
     const { currentStepIdx, formSubmitted } = useStore()
-    const aria = currentStepIdx + 1 === num ? { "aria-current": "step" } : {}
+    const aria: {} = currentStepIdx + 1 === num ? { "aria-current": "step" } : {}
 
     return (
-        <div className={`w-9 h-9 rounded-full grid place-items-center font-medium 
+        <div {...aria} className={`w-9 h-9 rounded-full grid place-items-center font-medium 
         ${currentStepIdx + 1 === num || formSubmitted && currentStepIdx === num ? 'bg-primary-100 text-primary-900' : 'bg-transparent text-white border border-neutral-100'} 
-        `} {...aria} >
+        `}>
             {num}
         </div>
     )
